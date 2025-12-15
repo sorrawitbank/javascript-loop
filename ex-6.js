f76a6aabfe91fc5c62111b234ea486b6fcf0c935
Bank <bank23113@gmail.com>
2025-12-15 14:50:16 +0700

    feat: finish ex6

--- ex-6.js
+++ ex-6.js
@@ -1,7 +1,24 @@
 // Exercise #6: Reverse the String
 let companyName = "TechUp Thailand";
-let reversedCompanyName = "";
+let reversedCompanyName1 = "";
+let reversedCompanyName2 = "";
+let reversedCompanyName3 = "";
 
 // Start coding here
+// Method 1
+for (const letter of companyName) {
+  reversedCompanyName1 = letter + reversedCompanyName1;
+}
+console.log(reversedCompanyName1);
 
-console.log(reversedCompanyName);
+// Method 2
+for (let i = 0; i < companyName.length; i++) {
+  reversedCompanyName2 = companyName[i] + reversedCompanyName2;
+}
+console.log(reversedCompanyName2);
+
+// Method 3
+for (let i = companyName.length - 1; i >= 0; i--) {
+  reversedCompanyName3 += companyName[i];
+}
+console.log(reversedCompanyName3);
